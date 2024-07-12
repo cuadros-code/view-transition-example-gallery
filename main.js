54d76330168a6a69cd1e71d83bce86327451f536
kevin cuadros <kevindavidcuadros@gmail.com>
2024-07-12 09:37:55 -0500

    Update main.js to handle view transitions and set the image source on click event

--- main.js
+++ main.js
@@ -11,6 +11,11 @@ async function onClick(event) {
 
   document.querySelector('.main-card').style.viewTransitionName = 'transition-gallery';
 
+  if(!document.startViewTransition){
+    document.querySelector('.main-card').src = event.target.src;
+    return
+  }
+
   const transition = document.startViewTransition(() => {
     document.querySelector('.main-card').src = event.target.src;
   })
